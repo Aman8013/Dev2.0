a6bfc2935f2f740140d2813656a9a612f9ff2d04
Aman Omkar <amanomkar823001@gmail.com>
2023-08-26 03:50:39 +0530

    index file changed

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import bodyParser from "body-parser";
 const app=express();
 const port=3000;
 
-
-
-
-
-
-
-
+app.get("/",(req,res)=>{
+    res.send('<h1>hi</h1>');
+})
+app.get("/login",(req,res)=>{
+    console.log("hiii");
+    res.send('<h1>Hello</h1>');
+})
+app.get("/register",(req,res)=>{
+    res.send('<h1>Hello</h1>');
+})
 app.listen(port,(req,res)=>{
   console.log(`server is running at port ${port}`);
 })
