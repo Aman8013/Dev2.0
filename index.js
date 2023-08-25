047fa158459d4cd167e98ec7a40343a51d648bcc
Aakash Kaushik <kaushikaakash527@gmail.com>
2023-08-26 04:21:26 +0530

    data connect

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
+import 'dotenv/config'
 import express from "express";
 import bodyParser from "body-parser";
-const app=express();
-const port=3000;
+import mongoose from "mongoose";
 
-app.get("/",(req,res)=>{
+const app = express();
+const DB = process.env.DB;
+const PORT = process.env.PORT
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(DB, { useNewUrlParser: true }).then(() => {
+    console.log('database connected');
+}).catch(err => {
+    console.log(err);
+})
+
+
+app.get("/", (req, res) => {
     res.send('<h1>hi</h1>');
 })
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     console.log("hiii");
     res.send('<h1>Hello</h1>');
 })
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
     res.send('<h1>Hello</h1>');
 })
-app.listen(port,(req,res)=>{
-  console.log(`server is running at port ${port}`);
+app.listen(PORT, () => {
+    console.log(`server is running at port ${PORT}`);
 })
