e239f1b9a36c66e1e4288da706548b1748b2ad1a
Ashutosh Awasthi <ashutoshawasthi66@gmail.com>
2023-08-27 03:16:46 +0530

    added create new blog frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from './Components/Login';
+import New from './Components/Blog/New';
 
 function App() {
   return (
     <>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/blog/new" element={<New />} />
           {/* <Route path="/register" element={<Register />} /> */}
           {/* <Route path="*" element={<Error />} /> */}
         </Routes>
