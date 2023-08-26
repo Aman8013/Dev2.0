440a8984907bd9ccd35c6067790f12114a8b1923
Aakash Kaushik <kaushikaakash527@gmail.com>
2023-08-27 02:03:12 +0530

    add restful route

--- index.js
+++ index.js
@@ -15,16 +15,46 @@ mongoose.connect(DB, { useNewUrlParser: true }).then(() => {
 })
 
 
-app.get("/", (req, res) => {
-    res.send('<h1>hi</h1>');
+// app.get("/", (req, res) => {
+//     res.send('<h1>hi</h1>');
+// })
+
+// app.get("/login", (req, res) => {
+//     console.log("hiii");
+//     res.send('<h1>Hello</h1>');
+// })
+
+// app.get("/register", (req, res) => {
+//     res.send('<h1>Hello</h1>');
+// })
+
+
+// Blogs end points goes here
+// /blog post
+app.post('/blog', (req, res) => {
+
 })
-app.get("/login", (req, res) => {
-    console.log("hiii");
-    res.send('<h1>Hello</h1>');
+
+// get all blogs
+app.get('/blog', (req, res) => {
+
 })
-app.get("/register", (req, res) => {
-    res.send('<h1>Hello</h1>');
+
+// /blog/id get
+app.get('/blog/:id', (req, res) => {
+
 })
+
+// /blog/id put
+app.patch('/blog/:id/edit', (req, res) => {
+
+})
+
+// /blog/id delete
+app.delete('/blog/:id', (req, res) => {
+
+})
+
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`);
 })
