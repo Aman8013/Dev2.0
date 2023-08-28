17b2663085495b49351a3551ba65d86a23dbab1e
Aman Omkar <amanomkar823001@gmail.com>
2023-08-29 00:39:12 +0530

    user login untested

--- routes/Aroute.js
+++ routes/Aroute.js
@@ -9,12 +9,34 @@ const router = express.Router();
 // router.get("/register", async (req, res) => {
 //     try {
 //         const { username, email, password } = req.body;
-        
+
 //     } catch (error) {
 
 //     }
 
 // });
 // sigin user
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const foundItem = await Blog.findOne({ email: email });
+        const result = await bcrypt.compare(password, foundItem.password, 10);
+        if (result) {
+            const payload = {
+                name: foundItem.name,
+                email: foundItem.email,
+                id: foundItem._id
+            }
+            const token = await jwt.sign(payload, process.env.JWTSECRET);
+            res.cookie('AUTH', token, { httpOnly: true });
+            res.status(200).json({ result: true });
+        } else {
+            res.status(200).json({ result: false });
+        }
+    } catch (error) {
+        res.status(500).json("internal server error");
+    }
+})
 
 // logout
