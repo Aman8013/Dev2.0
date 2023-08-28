fecb9ea72306e4eb2e0fa0b767ba895035bcbf82
Ashutosh Awasthi <ashutoshawasthi66@gmail.com>
2023-08-28 22:45:38 +0530

    using antd for styling signin and register pages are done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import New from './Components/Blog/New';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Update from './Components/Blog/Update';
 import Signin from './Components/Signin';
+import Register from './Components/Register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/blog/new" element={<New />} />
           <Route path="/blog/:id/edit" element={<Update />} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/register" element={<Register />} />
           {/* <Route path="*" element={<Error />} /> */}
         </Routes>
     </>
--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -1,11 +1,13 @@
 import React, { useRef } from 'react'
 import { Button, Row, Form, Input, Col, Card } from 'antd'
 
-const Signin = () => {
+const Register = () => {
     const email = useRef();
+    const fname = useRef();
     const password = useRef();
+    const cpassword = useRef();
 
-    const HandleSignin = (e) => {
+    const HandleRegister = (e) => {
         e.preventDefault()
         console.log(email.current.input.value)
         console.log(password.current.input.value)
@@ -35,6 +37,19 @@ const Signin = () => {
                             autoComplete="off"
                         >
                             <Form.Item
+                                label="Full Name"
+                                name="fname"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your Full Name!',
+                                    },
+                                ]}
+                            >
+                                <Input ref={fname} />
+                            </Form.Item>
+
+                            <Form.Item
                                 label="Email"
                                 name="email"
                                 rules={[
@@ -59,13 +74,37 @@ const Signin = () => {
                             >
                                 <Input.Password ref={password} />
                             </Form.Item>
+
+                            <Form.Item
+                                name="confirm"
+                                label="Confirm Password"
+                                dependencies={['password']}
+                                hasFeedback
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please confirm your password!',
+                                    },
+                                    ({ getFieldValue }) => ({
+                                        validator(_, value) {
+                                            if (!value || getFieldValue('password') === value) {
+                                                return Promise.resolve();
+                                            }
+                                            return Promise.reject(new Error('The new password that you entered do not match!'));
+                                        },
+                                    }),
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
                             <Form.Item
                                 wrapperCol={{
                                     offset: 8,
                                     span: 16,
                                 }}
                             >
-                                <Button type="primary" htmlType="submit" onClick={HandleSignin}>
+                                <Button type="primary" htmlType="submit" onClick={HandleRegister}>
                                     Submit
                                 </Button>
                             </Form.Item>
@@ -77,4 +116,4 @@ const Signin = () => {
     )
 }
 
-export default Signin
+export default Register
