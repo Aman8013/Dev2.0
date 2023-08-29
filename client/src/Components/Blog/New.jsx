9a0b7fe3c9384864a66cd580206127cc77fe0d98
Ashutosh Awasthi <ashutoshawasthi66@gmail.com>
2023-08-29 19:30:11 +0530

    frontend post page

--- client/src/Components/Blog/New.jsx
+++ client/src/Components/Blog/New.jsx
@@ -1,4 +1,24 @@
 import { useRef } from "react";
+import { UploadOutlined } from '@ant-design/icons';
+import {
+    Button,
+    Row,
+    Col,
+    DatePicker,
+    Form,
+    Input,
+    Card,
+    Radio,
+    Select,
+    Slider,
+    Switch,
+    TreeSelect,
+    Upload,
+    Space,
+} from 'antd';
+import ReactPlayer from 'react-player'
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
 
 const New = () => {
     const head = useRef();
@@ -21,24 +41,36 @@ const New = () => {
 
     return (
         <>
-            <div className="container mt-4 d-flex justify-content-center">
-                <div className="card p-3" style={{ width: "50rem" }}>
-                    <form className="row g-3">
-                        <div className="col-md-12">
-                            <label htmlFor="title" className="form-label">Title</label>
-                            <input type="text" className="form-control" id="title" name="head" ref={head} />
-                        </div>
-                        <div className="col-12">
-                            <label htmlFor="body" className="form-label">Body</label>
-                            <textarea className="form-control" rows="20" ref={body}></textarea>
-                        </div>
+            <ReactPlayer url='https://www.twitch.tv/videos/1909175634' playing={true} />
+            <Row justify='center' style={{ height: '100vh', marginTop: '2rem' }}>
+                <Col md={12} span={22}>
+                    <Card
+                        title="New Post"
+                    >
+                        <Form>
+                            <Form.Item label="Title">
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="Body">
+                                <TextArea rows={20} />
+                            </Form.Item>
+                            <Form.Item wrapperCol={{
+                                offset: 2,
+                                span: 16,
 
-                        <div className="col-12">
-                            <button type="submit" className="btn btn-primary" onClick={HandlePost}>POST</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                            }}>
+                                <Space>
+                                    <Button type="primary">POST</Button>
+                                    <Upload>
+                                        <Button icon={<UploadOutlined />}>Upload</Button>
+                                    </Upload>
+                                </Space>
+                            </Form.Item>
+
+                        </Form>
+                    </Card>
+                </Col>
+            </Row>
         </>
     )
 }
